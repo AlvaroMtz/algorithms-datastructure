05f9c19a5ebbd8c116d03c43c3003eafe38b31f7
AlvaroMtz <hola@alvaromtz.es>
2021-12-09 23:35:06 +0100

    BigO for javascript objects

--- bigO-objects.js
+++ bigO-objects.js
@@ -3,3 +3,8 @@ let instructor = {
     isInstructor: true,
     favouriteNumbers: [1,2,3,4]
 }
+//BigO of Objects (use if you dont need order)
+// Insertion -> O(1)
+// Removal -> O(1)
+// Searching -> O(n)
+// Access -> O(1)
