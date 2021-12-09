cc762873f4835faa6e5c29a72c92dec3d9c8c8d5
AlvaroMtz <hola@alvaromtz.es>
2021-12-09 23:43:45 +0100

    BigO for javascript arrays

---
+++ bigO-arrays.js
@@ -0,0 +1,8 @@
+let names = ["Michael", "Melisa", "Andrea"]
+let values = [true, {}, [], 2, "awesome"]
+
+//BigO of Arrays (if you need order)
+// Insertion -> It depends
+// Removal -> It depends
+// Searching -> O(n)
+// Access -> O(1)
